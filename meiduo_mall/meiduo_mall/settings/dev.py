462882fa583362693d9a208e8c9064f1e0fc9dfc
shehuizhou <1476848055@qq.com>
2019-04-22 19:27:05 +0800

    配置文件

--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -1,3 +1,4 @@
+#生产文件
 """
 Django settings for meiduo_mall project.
 
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'meiduo_mall.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '../../db.sqlite3'),
     }
 }
 
